ec1023c29a1971d938f389b68558fa63aca32b97
Ahmed ali khan <159822322+James08923@users.noreply.github.com>
2024-04-14 18:22:19 +0500

    Assignment no 23

--- app.js
+++ app.js
@@ -1,130 +1,144 @@
-function calculateValues() {
-    // Get the input value
-    var number = parseInt(document.getElementById("numberInput").value);
+var now = new Date();
+var datetimeElement = document.getElementById('datetime');
+// datetimeElement.textContent = now.toLocaleString();
 
-    // Display the number
-    document.getElementById("number").innerText = "Number: " + number;
 
-    // Calculate and display round off value
-    document.getElementById("roundOff").innerText = "Round off value: " + Math.round(number);
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var now = new Date();
+var currentMonth = months[now.getMonth()];
+alert("Current month is: " + currentMonth);
 
-    // Calculate and display floor value
-    document.getElementById("floorValue").innerText = "Floor value: " + Math.floor(number);
 
-    // Calculate and display ceil value
-    document.getElementById("ceilValue").innerText = "Ceil value: " + Math.ceil(number);
+var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var now = new Date();
+var currentDayIndex = now.getDay();
+var currentDayAbbreviation = days[currentDayIndex];
+alert("Today is: " + currentDayAbbreviation);
+
+
+var now = new Date();
+var currentDayIndex = now.getDay();
+
+if (currentDayIndex === 0 || currentDayIndex === 6) {
+    alert("It's Fun day!");
+} else {
+    alert("It's Fun day. Keep going!");
 }
 
 
+var now = new Date();
+            var currentDate = now.getDate();
 
-
-
-function calculateValues() {
-    // Get the input value
-    var number = parseFloat(document.getElementById("numberInput").value);
-
-    // Display the number
-    document.getElementById("number").innerText = "Number: " + number;
-
-    // Calculate and display round off value
-    document.getElementById("roundOff").innerText = "Round off value: " + Math.round(number * 100) / 100;
-
-    // Calculate and display floor value
-    document.getElementById("floorValue").innerText = "Floor value: " + Math.floor(number);
-
-    // Calculate and display ceil value
-    document.getElementById("ceilValue").innerText = "Ceil value: " + Math.ceil(number);
-}
-
-
-
-
-function calculateAbsoluteValue() {
-    // Get the input value
-    var number = parseFloat(document.getElementById("numberInput").value);
-
-    // Calculate absolute value
-    var absoluteValue = Math.abs(number);
-
-    // Display the absolute value
-    document.getElementById("absoluteValue").innerText = "Absolute Value: " + absoluteValue;
-}
-
-
-
-function rollDice() {
-    // Generate a random number between 1 and 6 for the dice
-    var diceValue = Math.floor(Math.random() * 6) + 1;
-
-    // Display the dice value
-    document.getElementById("diceValue").innerText = "Dice Value: " + diceValue;
-}
-
-
-
-function tossCoin() {
-    // Generate a random number between 0 and 1 for the coin toss
-    var tossResult = Math.random();
-
-    // If the result is less than 0.5, it's heads, otherwise it's tails
-    var coinValue = (tossResult < 0.5) ? "Heads" : "Tails";
-
-    // Display the coin value
-    document.getElementById("coinValue").innerText = "Coin Value: " + coinValue;
-}
-
-
-    // Generate a random number between 1 and 100
-    var randomNumber = Math.floor(Math.random() * 100) + 1;
-
-    // Display the random number
-    document.getElementById("randomNumber").innerText = "Random Number: " + randomNumber;
-
-
-
-
-        // Get the user input
-        var userInput = document.getElementById("weightInput").value.trim().toLowerCase();
-    
-        // Regular expression to match the weight format
-        var regex = /^(\d+(\.\d+)?)\s*(kg|kilograms?)?$/;
-    
-        // Match the user input against the regular expression
-        var match = userInput.match(regex);
-    
-        // If a match is found
-        if (match) {
-            // Extract the weight value
-            var weight = parseFloat(match[1]);
-    
-            // Display the weight
-            document.getElementById("weight").innerText = "Weight: " + weight + " kgs";
-        } else {
-            // Display an error message for invalid input
-            document.getElementById("weight").innerText = "Invalid input. Please enter weight in the format: 50, 50kgs, 50.2kgs, or 50.2kilograms";
-        }
-
-
-
-
-    
-            // Generate a random secret number between 1 and 10
-            var secretNumber = Math.floor(Math.random() * 10) + 1;
-        
-            // Get the user input
-            var userInput = parseInt(document.getElementById("userInput").value);
-        
-            // Check if the user input equals the secret number
-            if (userInput === secretNumber) {
-                // If user guessed correctly, congratulate
-                document.getElementById("result").innerText = "Congratulations! You guessed the secret number.";
+            if (currentDate <= 15) {
+                alert("First fifteen days of the month");
             } else {
-                // If user guessed incorrectly, provide feedback
-                document.getElementById("result").innerText = "Sorry, the secret number was " + secretNumber + ". Try again!";
+                alert("Last days of the month");
             }
 
 
+var currentDate = new Date();
+
+
+var minutesSinceEpoch = currentDate.getTime() / (1000 * 60);
+
+
+console.log("Minutes since midnight, Jan. 1, 1970:", minutesSinceEpoch);
+
+
+var now = new Date();
+var currentHour = now.getHours();
+
+if (currentHour < 12) {
+    alert("It's AM");
+} else {
+    alert("It's PM");
+}
+
+
+var laterDate = new Date(2020, 11, 31);
+
+
+console.log("Later Date:", laterDate);
+
+
+var ramadanStartDate = new Date(2015, 5, 18); 
+
+
+var currentDate = new Date();
+
+var timeDifference = currentDate.getTime() - ramadanStartDate.getTime();
+
+
+var daysSinceRamadanStart = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+alert("Number of days past since 1st Ramadan: " + daysSinceRamadanStart);
 
 
 
-        
+        var beginningOf2015 = new Date(2015, 0, 1, 0, 0, 0);
+
+        var referenceDate = new Date();
+
+        var secondsElapsed = Math.floor((referenceDate - beginningOf2015) / 1000);
+
+        document.write("Seconds elapsed since the beginning of 2015: " + secondsElapsed);
+
+
+       
+        var currentDate = new Date();
+
+      
+        var currentHours = currentDate.getHours();
+
+   
+        currentDate.setHours(currentHours + 1);
+
+
+        document.write("Updated Date Object: " + currentDate);
+
+
+         var currentDate = new Date();
+
+         currentDate.setFullYear(currentDate.getFullYear() - 100);
+
+         alert("Date 100 years back: " + currentDate);
+
+
+
+        var age = prompt("Please enter your age:");
+
+        var currentYear = new Date().getFullYear();
+        var birthYear = currentYear - parseInt(age);
+
+        document.write("Your birth year is: " + birthYear);
+
+
+
+
+
+        var customerName = prompt("Enter customer name:");
+        var currentMonth = prompt("Enter current month:");
+        var numberOfUnits = parseFloat(prompt("Enter number of units:"));
+        var chargesPerUnit = parseFloat(prompt("Enter charges per unit:"));
+        var latePaymentSurchargeRate = parseFloat(prompt("Enter late payment surcharge rate (in %):"));
+
+  
+        var netAmountPayable = numberOfUnits * chargesPerUnit;
+
+    
+        var latePaymentSurcharge = (latePaymentSurchargeRate / 100) * netAmountPayable;
+
+        var grossAmountPayable = netAmountPayable + latePaymentSurcharge;
+
+        netAmountPayable = netAmountPayable.toFixed(2);
+        latePaymentSurcharge = latePaymentSurcharge.toFixed(2);
+        grossAmountPayable = grossAmountPayable.toFixed(2);
+
+        document.write("<h2>K-Electric Bill</h2>");
+        document.write("<p>Customer Name: " + customerName + "</p>");
+        document.write("<p>Current Month: " + currentMonth + "</p>");
+        document.write("<p>Number of Units: " + numberOfUnits + "</p>");
+        document.write("<p>Charges Per Unit: " + chargesPerUnit.toFixed(2) + "</p>");
+        document.write("<p>Net Amount Payable (within Due Date): " + netAmountPayable + "</p>");
+        document.write("<p>Late Payment Surcharge: " + latePaymentSurcharge + "</p>");
+        document.write("<p>Gross Amount Payable (after Due Date): " + grossAmountPayable + "</p>");
